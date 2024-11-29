561d67683671b01036a03ba386991601def338ff
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-11-30 00:42:13 +0500

    Add project files.

---
+++ Tyuiu.DmitrievLR.Sprint1.Task0.V12.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.DmitrievLR.Sprint1.Task0.V12.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint1.Task0.V12/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.DmitrievLR.Sprint1.Task0.V12.Lib;
+namespace Tyuiu.DmitrievLR.Sprint1.Task0.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (5*2 - 2) / 4 * 3 и     *");
+
+            Console.WriteLine("* печатает результат на экране.                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (5*2 - 2) / 4 * 3                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
