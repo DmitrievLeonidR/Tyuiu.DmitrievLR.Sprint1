7a4fa04c322d37a8a6cc9ea6f89a25d8731a05ac
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-11-30 01:01:12 +0500

    Task1

---
+++ Tyuiu.DmitrievLR.Sprint1.Task1.V28/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.DmitrievLR.Sprint1.Task1.V28.Lib;
+namespace Tyuiu.DmitrievLR.Sprint1.Task1.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+
+            Console.WriteLine("* вычисляет результат по формуле (x*2)/(1+x) и печатает его на экране.    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+
+            Console.WriteLine(" Ведите значение X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(x);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
