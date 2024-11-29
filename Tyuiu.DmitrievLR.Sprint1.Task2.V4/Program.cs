c6f6109d51b73648cc412fa40968e75d57ed118f
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-11-30 01:38:36 +0500

    Task2

---
+++ Tyuiu.DmitrievLR.Sprint1.Task2.V4/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.DmitrievLR.Sprint1.Task2.V4.Lib;
+namespace Tyuiu.DmitrievLR.Sprint1.Task2.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: 1.2 Арифметические операторы в C#                                 *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите сторону квадрата: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.CalculateSquare(x);
+            Console.WriteLine("Площадь квадрата = " + result);
+            Console.ReadKey();
+        }
+    }
+}
