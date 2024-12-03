ab7174658284f3edf12e3584e98e8adfe51ac7ae
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-03 20:56:52 +0500

    Task4

---
+++ Tyuiu.DmitrievLR.Sprint1.Task4.V29/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.DmitrievLR.Sprint1.Task4.V29.Lib;
+namespace Tyuiu.DmitrievLR.Sprint1.Task4.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные *");
+
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Sqrt(2+ |x-2*y| ) / (3*x*y^2  )                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(x,y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
