ab442dc39593da8ebf8ba6437df4e4120115f966
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-03 23:03:25 +0500

    Task5

---
+++ Tyuiu.DmitrievLR.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.DmitrievLR.Sprint1.Task5.V2.Lib;
+namespace Tyuiu.DmitrievLR.Sprint1.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #2                                                            *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("* Дано значение температуры в градусах Фаренгейта. Определить значение этой же температуры  *");
+
+            Console.WriteLine("* в градусах Цельсия. Ответ привести к целому с помощью класса Convert. *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Введите значение температуры в градусах Фаренгейта:");
+            double FAR = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.FahrenheitToСelsius(FAR);
+            Console.WriteLine("Значение температуры в градусах цельсия: " + result);
+            Console.ReadKey();
+        }
+    }
+}
