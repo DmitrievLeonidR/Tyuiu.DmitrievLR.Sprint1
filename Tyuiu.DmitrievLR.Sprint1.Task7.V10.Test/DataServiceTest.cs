32420b95260066bd07c64c6f1fcedc6a8f5921cd
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-04 16:32:53 +0500

    Task7

---
+++ Tyuiu.DmitrievLR.Sprint1.Task7.V10.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.DmitrievLR.Sprint1.Task7.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint1.Task7.V10/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.DmitrievLR.Sprint1.Task7.V10.Lib;
+namespace Tyuiu.DmitrievLR.Sprint1.Task7.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по исходным  *");
+
+            Console.WriteLine("* значениям данных, вводимых пользователем. Ответ округлите до 3 знаков после запятой. *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Введите значение x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*z =  2 * ctg(3 * x) - (Log(Cos(x)) / Log(1 + x^2))                       *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(x);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
